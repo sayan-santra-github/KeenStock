dcbf3489cbc5fc508cfc044ad4b81ca860a3b57e
Sayan <primarysayan@gmail.com>
2025-05-23 12:07:47 +0530

    create user route

--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,14 @@ const express = require('express');
 const app = express()
 const cors = require('cors')
 
+const mainRouter = require('./router/mainRoute');
+const userRouter = require('./router/userRoute')
+
+
 app.use(cors);
+
+
+app.use('/user', userRouter);
 
 const port = process.env.PORT || 8990;
 app.listen(port, () => {
